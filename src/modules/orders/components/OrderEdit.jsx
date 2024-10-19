fc60e16aeb49981862f1d51ec977aa74528cb310
abdinazarov7799 <parolxato@gmail.com>
2024-10-20 01:27:41 +0500

    fix: order edit

--- src/modules/orders/components/OrderEdit.jsx
+++ src/modules/orders/components/OrderEdit.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {KEYS} from "../../../constants/key.js";
 import {URLS} from "../../../constants/url.js";
 import {get} from "lodash";
 import useGetOneQuery from "../../../hooks/api/useGetOneQuery.js";
-import {Divider, Row, Space, Typography} from "antd";
+import {Button, Col, Divider, Image, InputNumber, Popconfirm, Row, Select, Space, Spin, Typography} from "antd";
 import {useTranslation} from "react-i18next";
 import dayjs from "dayjs";
-import EditedProduct from "./EditedProduct.jsx";
+import {CloseOutlined} from "@ant-design/icons";
+import useGetAllQuery from "../../../hooks/api/useGetAllQuery.js";
+import usePatchQuery from "../../../hooks/api/usePutQuery.js";
 const {Text,Title} = Typography;
 
-const OrderEdit = ({selected,getStatusColor}) => {
+const OrderEdit = ({selected,setSelected,getStatusColor}) => {
     const id = get(selected,'id')
     const {t} = useTranslation()
+    const [order,setOrder] = useState(selected)
+    const [search,setSearch] = useState(null)
+    const [selectedProducts,setSelectedProducts] = useState([]);
     const {data,isLoading} = useGetOneQuery({
         id,
         key: `${KEYS.order_products}_${id}`,
@@ -21,6 +26,86 @@ const OrderEdit = ({selected,getStatusColor}) => {
             size: 1000
         }
     })
+    const {data:products,isLoading:isLoadingProducts,refetch:refetchProducts} = useGetAllQuery({
+        key: KEYS.product_list,
+        url: URLS.product_list,
+        params: {
+            params: {
+                search,
+                page: 0,
+                size: 50
+            }
+        }
+    })
+    const {mutate,isLoading:isLoadingEdit} = usePatchQuery({
+        listKeyId: KEYS.order_list
+    })
+    useEffect(() => {
+        refetchProducts()
+    },[search])
+
+    useEffect(() => {
+        setOrder({
+            ...selected,
+            products: get(data,'data.content',[])
+        })
+    },[selected,products])
+
+    const onEditOrder = () => {
+        mutate({
+            url: `${URLS.order_edit}/${get(selected,'id')}`,
+            attributes: {
+                pharmacyPhoneNumber: get(selected,'phoneNumber'),
+                products: get(order,'products')?.map(product=> ({productId: get(product,'id'), quantity: get(product,'quantity')})),
+            }
+        },{
+            onSuccess: () => {
+                setSelected(null)
+            }
+        })
+    }
+
+    const deleteProduct = (id) => {
+        const updatedProducts = get(order,'products').filter(product => product.id !== id);
+        setOrder({
+            ...order,
+            products: updatedProducts
+        });
+    }
+
+    const onChange = (value,data) => {
+        setSelectedProducts(data);
+    }
+    const onSaveOrder = () => {
+        // Avoid duplicates
+        const updatedOrderProducts = [...get(order, 'products')];
+
+        selectedProducts.forEach(selectedProduct => {
+            const existingProduct = updatedOrderProducts.find(product => product.id === selectedProduct.value);
+            if (!existingProduct) {
+                updatedOrderProducts.push({
+                    ...get(selectedProduct, 'productData'),
+                    quantity: 1
+                });
+            }
+        });
+
+        setOrder({
+            ...order,
+            products: updatedOrderProducts
+        });
+        setSelectedProducts([]);
+    };
+
+    const onChangeQuantity = (id, quantity) => {
+        const updatedProducts = get(order, 'products', []).map(product =>
+            product.id === id ? { ...product, quantity } : product
+        );
+        setOrder({
+            ...order,
+            products: updatedProducts
+        });
+    };
 
     return (
         <div>
@@ -28,33 +113,100 @@ const OrderEdit = ({selected,getStatusColor}) => {
             <Space direction={"vertical"} style={{width: "100%"}} size={"middle"}>
                 <Row justify={"space-between"} align={"middle"}>
                     <Title level={5}>{t("Address")}</Title>
-                    <Text>{get(selected,'address')}</Text>
+                    <Text>{get(order,'address')}</Text>
                 </Row>
                 <Row justify={"space-between"} align={"middle"}>
                     <Title level={5}>{t("Pharmacy")}</Title>
-                    <Text>{get(selected,'pharmacy')}</Text>
+                    <Text>{get(order,'pharmacy')}</Text>
                 </Row>
                 <Row justify={"space-between"} align={"middle"}>
                     <Title level={5}>{t("Phone")}</Title>
-                    <Text>{get(selected,'phoneNumber')}</Text>
+                    <Text>{get(order,'phoneNumber')}</Text>
                 </Row>
                 <Row justify={"space-between"} align={"middle"}>
                     <Title level={5}>{t("Created time")}</Title>
-                    <Text>{dayjs(get(selected,'createdTime')).format("YYYY-MM-DD HH:mm:ss")}</Text>
+                    <Text>{dayjs(get(order,'createdTime')).format("YYYY-MM-DD HH:mm:ss")}</Text>
                 </Row>
                 <Row justify={"space-between"} align={"middle"}>
                     <Title level={5}>{t("Total price")}</Title>
-                    <Text>{Number(get(selected,'totalPrice')).toLocaleString("en-US")} {t("so'm")}</Text>
+                    <Text>{Number(get(order,'totalPrice')).toLocaleString("en-US")} {t("so'm")}</Text>
                 </Row>
                 <Row justify={"space-between"} align={"middle"}>
                     <Title level={5}>{t("Status")}</Title>
-                    <Text type={getStatusColor(get(selected,'status'))}>{get(selected,'status')}</Text>
+                    <Text type={getStatusColor(get(order,'status'))}>{get(order,'status')}</Text>
                 </Row>
-                {
-                    get(data,'data.content',[])?.map(product => {
-                        return <EditedProduct product={product} key={get(product,'id')}/>
-                    })
-                }
+                <Spin spinning={isLoading}>
+                    <Space direction={"vertical"} style={{width: "100%"}} >
+                        {
+                            get(order,'products',[])?.map(product => {
+                                return (
+                                    <Row gutter={10} key={get(product,'id')}>
+                                        <Col span={2}>
+                                            <Image src={get(product,'imageUrl')} width={50} height={50} />
+                                        </Col>
+                                        <Col span={12}>
+                                            <Text ellipsis>{get(product,'name')}{get(product,'name')}{get(product,'name')}{get(product,'name')}{get(product,'name')}{get(product,'name')}</Text>
+                                        </Col>
+                                        <Col>
+                                            <Text>{get(product,'price')?.toLocaleString("en-US")} {t("so'm")}</Text>
+                                        </Col>
+                                        <Col span={3}>
+                                            <InputNumber
+                                                style={{width:80}}
+                                                type={"number"}
+                                                value={get(product,'quantity')}
+                                                min={0}
+                                                onChange={(value) => onChangeQuantity(get(product, 'id'), value)}
+                                            />
+                                        </Col>
+                                        <Col span={3}>
+                                            <Text>{Number(get(product,'price') * get(product,'quantity'))?.toLocaleString("en-US")} {t("so'm")}</Text>
+                                        </Col>
+                                        <Col span={1}>
+                                            <Popconfirm
+                                                title={t("Delete")}
+                                                description={t("Are you sure to delete")}
+                                                onConfirm={() => deleteProduct(get(product,'id'))}
+                                                okText={t("Yes")}
+                                                cancelText={t("No")}
+                                            >
+                                                <Button danger icon={<CloseOutlined />}/>
+                                            </Popconfirm>
+                                        </Col>
+                                    </Row>
+                                )
+                            })
+                        }
+                    </Space>
+                </Spin>
+                <Row gutter={30}>
+                    <Col span={20}>
+                        <Select
+                            showSearch
+                            placeholder={t("Select a medicine")}
+                            optionFilterProp="label"
+                            onChange={onChange}
+                            mode="multiple"
+                            style={{width: "100%"}}
+                            value={selectedProducts}
+                            loading={isLoadingProducts}
+                            onSearch={(value) => setSearch(value)}
+                            options={get(products,'data.content',[])?.map(product => {
+                                return {
+                                    label: get(product,'name'),
+                                    value: get(product,'id'),
+                                    productData: product
+                                }
+                            })}
+                        />
+                    </Col>
+                    <Col span={4}>
+                        <Button type={"primary"} block onClick={onSaveOrder}>
+                            {t("Add")}
+                        </Button>
+                    </Col>
+                </Row>
+                <Button type={"primary"} block onClick={onEditOrder} loading={isLoadingEdit}>{t("Save")}</Button>
             </Space>
         </div>
     );
