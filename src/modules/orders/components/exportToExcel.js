5af1d1f714cbc7f6b63f459a7a1cc6fc3ef242f0
abdinazarov7799 <parolxato@gmail.com>
2024-12-11 03:10:27 +0500

    change: excel file

--- src/modules/orders/components/exportToExcel.js
+++ src/modules/orders/components/exportToExcel.js
@@ -1,51 +1,64 @@
 import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
-import {get} from "lodash";
+import { get } from "lodash";
 
 const exportToExcel = (data, fileName) => {
-    const formattedProducts = data.products.map((product, index) => ({
-        Index: index + 1,
-        Name: get(product, "name"),
-        ID: get(product, "id"),
-        Price: `${Intl.NumberFormat("en-US").format(get(product, "price",0))} so'm`,
-        Quantity: get(product, "quantity"),
-        ImageURL: get(product, "imageUrl"),
-    }));
-
     const generalInfo = [
-        { Field: "ID", Value: `${get(data,'id')}` },
-        { Field: "Full Name", Value: get(data,'fullName') },
-        { Field: "Address", Value: get(data,'address') },
-        { Field: "INN", Value: get(data,'inn') },
-        { Field: "Pharmacy", Value: get(data,'pharmacy') },
-        { Field: "Phone Number", Value: get(data,'phoneNumber') },
-        { Field: "User Phone", Value: get(data,'userPhone') },
-        { Field: "Status", Value: get(data,'status') },
-        { Field: "Total Price", Value: `${Intl.NumberFormat('en-US').format(get(data,'totalPrice',0))} so'm`, },
+        ["Full Name", get(data, "fullName")],
+        ["Address", get(data, "address")],
+        ["INN", get(data, "inn")],
+        ["Pharmacy", get(data, "pharmacy")],
+        ["Phone Number", get(data, "phoneNumber")],
+        ["User Phone", get(data, "userPhone")],
+        ["Total Price", `${Intl.NumberFormat("en-US").format(get(data, "totalPrice", 0))} so'm`],
     ];
 
-    const wsGeneral = XLSX.utils.json_to_sheet(generalInfo);
-    const wsProducts = XLSX.utils.json_to_sheet(formattedProducts);
+    const productHeader = ["№", "Name", "Price", "Quantity"];
+    const formattedProducts = data.products.map((product, index) => [
+        index + 1,
+        get(product, "name"),
+        `${Intl.NumberFormat("en-US").format(get(product, "price", 0))} so'm`,
+        get(product, "quantity"),
+    ]);
 
-    wsGeneral["!cols"] = [
-        { wch: 25 },
-        { wch: 40 },
+    const combinedData = [
+        ...generalInfo,
+        [],
+        [],
+        productHeader,
+        ...formattedProducts,
     ];
 
-    wsProducts["!cols"] = [
+    const ws = XLSX.utils.aoa_to_sheet(combinedData);
+
+    ws["!cols"] = [
         { wch: 10 },
-        { wch: 70 },
-        { wch: 10 },
+        { wch: 50 },
+        { wch: 20 },
         { wch: 15 },
-        { wch: 15 },
-        { wch: 100 },
     ];
+
+    const range = XLSX.utils.decode_range(ws["!ref"]);
+    for (let R = range.s.r; R <= range.e.r; ++R) {
+        for (let C = range.s.c; C <= range.e.c; ++C) {
+            const cell = ws[XLSX.utils.encode_cell({ r: R, c: C })];
+            if (cell) {
+                cell.s = {
+                    border: {
+                        top: { style: "thin", color: { rgb: "000000" } },
+                        bottom: { style: "thin", color: { rgb: "000000" } },
+                        left: { style: "thin", color: { rgb: "000000" } },
+                        right: { style: "thin", color: { rgb: "000000" } },
+                    },
+                };
+            }
+        }
+    }
 
     const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, wsGeneral, "General Info");
-    XLSX.utils.book_append_sheet(wb, wsProducts, "Products");
+    XLSX.utils.book_append_sheet(wb, ws, "Order Details");
 
-    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array", cellStyles: true });
     const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
     saveAs(blob, `${fileName}.xlsx`);
 };
